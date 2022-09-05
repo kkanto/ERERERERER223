f0cadb97dd88a09297395d174265bf5785307965
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-05 16:35:08 +0200

    Create gradle project

---
+++ src/main/java/de/allianz/ERERERERER223/Ererererer223Application.java
@@ -0,0 +1,13 @@
+package de.allianz.ERERERERER223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ererererer223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ererererer223Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/allianz/ERERERERER223/Ererererer223ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.allianz.ERERERERER223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ererererer223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
